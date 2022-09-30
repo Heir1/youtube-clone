5867c7345f656ef2f7b2f3e3465bb7f380a25e59
Heir1 <nkeleheir@gmail.com>
2022-09-30 11:41:37 +0100

    end of sidebar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap';
 import Header from './Components/Header/Header';
 import Sidebar from './Components/Siderbar/Sidebar';
@@ -7,17 +7,31 @@ import './_app.scss';
 
 
 const App = () =>{
-  return (
-    <>
-        <Header/>
-        <div className='app__container border border-info'>
-            <Sidebar/>
-            <Container fluid className='app__main border border-warning' >
-                <HomeScreen/>
-            </Container>
-        </div>       
-    </>
-  )
+
+    const [sidebar,toggleSidebar] = useState(false);
+
+    const handleToggleSidebar = () => {
+        toggleSidebar(!sidebar)
+    }
+
+    // <div className='app__container border border-info'>
+    // border border-warning
+
+    return (
+        <>
+            <Header handleToggleSidebar={handleToggleSidebar} />
+            
+            <div className='app__container'>
+                <Sidebar 
+                    sidebar={sidebar} 
+                    handleToggleSidebar={handleToggleSidebar} 
+                />
+                <Container fluid className='app__main' >
+                    <HomeScreen/>
+                </Container>
+            </div>       
+        </>
+    )
 }
 
 export default App;
